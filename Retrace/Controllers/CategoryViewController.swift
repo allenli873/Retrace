c2c6d85b63cf04c1f25fd4d425bd736dcbc553b3
Allen Li <allenli873@gmail.com>
2020-04-03 13:52:52 -0700

    Added Categorization Functionality
    Added the category entity with attribute name, allows users to create categories

--- Retrace/Controllers/CategoryViewController.swift
+++ Retrace/Controllers/CategoryViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
 
+    var categories = [Category]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadData()
     }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Category"
+            textField = alertTextField
+        }
+        let action = UIAlertAction(title: "Add Category", style: .default) { (alertAction) in
+            let text = textField.text!
+            // do nothing if user does not enter text
+            if text == "" {
+                return
+            }
+            
+            let newCategory = Category(context: K.context)
+            newCategory.name = text
+            self.categories.append(newCategory)
+            self.saveData()
+            self.tableView.reloadData()
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+}
 
+//MARK: - Data Manipulation Methods
 
+extension CategoryViewController {
+    func saveData() {
+        do {
+            try K.context.save()
+        } catch {
+            print("Error saving category context: \(error)")
+        }
+    }
+    
+    func loadData() {
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        do {
+            categories = try K.context.fetch(request)
+        } catch {
+            print("Error fetching category: \(error)")
+        }
+    }
+}
+
+//MARK: - Table View Data Source Methods
+
+extension CategoryViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.categoryIdentifier, for: indexPath)
+        cell.textLabel?.text = categories[indexPath.row].name
+        return cell
+    }
 }
 
---
+++ Retrace/Supporting/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Retrace
+//
+//  Created by Allen Li on 4/3/20.
+//  Copyright © 2020 Allen Li. All rights reserved.
+//
+
+import UIKit
+
+struct K {
+    static let categoryIdentifier = "categoryCell"
+    static let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+}
